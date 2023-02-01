fd8bc51e7a24a4eac9e99c167657940f60218e2c
kkala <kkala@microsoft.com>
2023-02-01 20:50:23 +0530

    solution 4 Paranthesis Checker

--- Week-8/4.ParenthesisChecker.js
+++ Week-8/4.ParenthesisChecker.js
@@ -1,3 +1,76 @@
-function parenthesisChecker(){
+"use strict";
+// function check given parathesis is balanced or not
+function parenthesisChecker(paranthesisArr){
+    // create a new stack
+    let st= new stack()
+    // iterate through each bracket
+    for(let i=0;i<paranthesisArr.length;i++){
+        // if bracket is opening bracket push it into the stack
+        if(paranthesisArr[i]==="(" || paranthesisArr[i]==="["|| paranthesisArr[i]==="{"){
+            st.push(arr[i])
+        }
+        else{
+            // if it closing brcaket pop the elment from stack 
+            // Do reverse of poped elment
+            let rev=ReverseOfBracket(paranthesisArr[i]);
+            if(rev === st.pop())continue;
+            else return false;
+            
+        }
+    }
+    if(st.IsEmpty()===true) return true;
+    return false;
 
 }
+// function for finding closing symbol of openinig bracket 
+function ReverseOfBracket(el){
+    if(el=")") return "(";
+    else if(el="]") return "[";
+    else if(el="}") return "{";
+}
+// stack implemetation
+class stack {
+    constructor(){
+        this.items=[];
+        this.top=-1;
+        
+    }
+    push(el){
+        this.top++
+        this.item[0]=el;
+        
+
+    }
+    pop(){
+        if(this.top===-1){
+            console.log("stack is empty")
+            return
+        }
+        let x= this.item[this.top];
+        this.item[this.top]=undefined;
+        this.top--
+
+    }
+    peek(){
+        return this.item[this.top]
+    }
+    IsEmpty(){
+        if(this.top===-1){
+            console.log("stack is empty");
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
+let paranthesisArr=["{[{]}"];
+console.log(parenthesisChecker(paranthesisArr));
+// function MakeStack(arr){
+//     st = new stack();
+//     for(i=0;i<arr.length;i++){
+//         st.push(arr[i]);
+//     }
+// }
+
